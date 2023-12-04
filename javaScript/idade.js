2ce41e15170845614236f10112e3834eb24d80fb
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2023-12-04 20:58:52 -0300

    Atividade 01 Idade Funcional

--- javaScript/idade.js
+++ javaScript/idade.js
@@ -1,2 +1,28 @@
-const idade = parseFloat(document.formIdade.idade.value);
-let resultado;
+function calcularIdade() {
+    let idade = parseFloat(document.formIdade.idade.value);
+    let resultado;
+
+    if (idade < 18) {
+        resultado = 'Menor';
+    }
+
+    else if (idade < 25) {
+        resultado = 'Jovem';
+    }
+
+    else if (idade < 45) {
+        resultado = 'Jovem Adulto';
+    }
+
+    else if (idade < 60) {
+        resultado = 'Adulto';
+    }
+
+    else {
+        resultado = 'Idoso';
+    }
+
+    const caixaResultado = document.getElementById('resultado');
+    caixaResultado.value = resultado; 
+
+}
